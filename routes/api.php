f8ed4a5c3f363d8af335c69b43dbb82e67c13c58
Anton Ivanilov <antonivanilov21@gmail.com>
2020-06-24 16:10:49 +0300

    installed nlpserver

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/users', function () {
     return factory('App\User', 10)->make();
 });
+Route::get('/testText', 'TestController@index');
